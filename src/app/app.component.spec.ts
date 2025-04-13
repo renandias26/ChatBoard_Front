5c4462253d18d4bd5027be32a97e5a4c346faf81
Renan Dias <renandias26@outlook.com.br>
2025-04-13 00:45:41 -0300

    Update project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ChatHub_Front' title`, () => {
+  it(`should have the 'ChatBoard_Front' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ChatHub_Front');
+    expect(app.title).toEqual('ChatBoard_Front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ChatHub_Front');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ChatBoard_Front');
   });
 });
